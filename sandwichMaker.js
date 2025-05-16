62391b0264a8fa5fdf1f755ade8adf8877779f49
christian <christian.cuneo@libero.it>
2025-05-16 13:04:14 +0200

    Make messages appear on HTML page

--- sandwichMaker.js
+++ sandwichMaker.js
@@ -1,23 +1,20 @@
 // JavaScript source code
 
-const fs = require('fs')
-const file = fs.createWriteStream('sandwichLog.txt')
-const failsLog = { 'outOfBread': 0, 'outOfLettuce': 0, 'breadBurned': 0, 'fallenChef' : 0 }
+
+const failsLog = { 'outOfBread': 0, 'outOfLettuce': 0, 'breadBurned': 0, 'fallenChef': 0 }
+const displayToDom = (text, tag = 'h2') => {
+    const el = document.createElement(tag)
+    el.innerHTML = text
+    document.body.appendChild(el)
+}
 
 const getBread = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const goinSouth = Math.random() > 0.6
             const outcome = goinSouth ? 'Outta Bread !!!' : 'Bread Ready'
-            if (!goinSouth) console.log(outcome)
-            file.write(`${outcome}\n`, () => {
-                if (goinSouth) {
-                    failsLog.outOfBread ++
-                    reject(outcome)
-                } else {
-                    resolve(outcome)
-                }
-            })
+            displayToDom(outcome)
+            goinSouth ? (failsLog.outOfBread++ , reject(outcome)): resolve(outcome)        
         }, 1000)
     })
 }
@@ -29,15 +26,9 @@ const addFillings = () => {
         setTimeout(() => {
             const goinSouth = Math.random() > 0.6
             const outcome = goinSouth ? 'Outta lettuce!!!' : 'Fillings added'
-            if (!goinSouth) console.log(outcome)
-            file.write(`${outcome}\n`, () => {
-                if (goinSouth) {
-                    failsLog.outOfLettuce ++
-                    reject(outcome)
-                } else {
-                    resolve(outcome)
-                }
-            })
+            displayToDom(outcome)
+            goinSouth ? (failsLog.outOfLettuce ++, reject(outcome)) : resolve(outcome) 
+            
         }, 1500)
     })
 }
@@ -47,15 +38,9 @@ const toastSandwich = () => {
         setTimeout(() => {
             const goinSouth = Math.random() > 0.6
             const outcome = goinSouth ? 'Bread Burned!!!' : 'Sandwich Toasted'
-            if(!goinSouth) console.log(outcome)
-            file.write(`${outcome}\n`, () => {
-                if (goinSouth) {
-                    failsLog.breadBurned ++
-                    reject(outcome)
-                } else {
-                    resolve(outcome)
-                }
-            })
+            displayToDom(outcome)
+            goinSouth ? (failsLog.breadBurned++, reject(outcome)) : resolve(outcome)
+            
         },2000)
     })
 }
@@ -64,15 +49,9 @@ const serveSandwich = () => {
         setTimeout(() => {
             const chefFalls = Math.random() > 0.5
             const outcome = chefFalls ? 'Chef tripped and fell, sandwich got destroyed' : 'Sandwich Ready\n'
-            console.log(outcome)
-            file.write(`${outcome}\n`, () => {
-                if (chefFalls) {
-                    failsLog.fallenChef ++
-                    reject(outcome)
-                } else {
-                    resolve(outcome)
-                }
-            })
+            displayToDom(outcome)
+            chefFalls ? (failsLog.chefFalls ++, reject(outcome)) : resolve(outcome)
+          
         }, 3000)
 
     })
@@ -80,11 +59,11 @@ const serveSandwich = () => {
 const organizeFails = () => {
     return new Promise((resolve, reject) => {
         if (failsLog.breadBurned || failsLog.outOfBread || failsLog.outOfLettuce || failsLog.fallenChef) {
-            file.write(`Reason for failings: \n Out of bread: ${failsLog.outOfBread} \n Out of lettuce: ${failsLog.outOfLettuce}\n Bread burned: ${failsLog.breadBurned}\n Chef fell: ${failsLog.fallenChef}`)
+            displayToDom(`Reason for failings: \n Out of bread: ${failsLog.outOfBread} \n Out of lettuce: ${failsLog.outOfLettuce}\n Bread burned: ${failsLog.breadBurned}\n Chef fell: ${failsLog.fallenChef}`)
             resolve()
         } else {
             const reason = 'First attempt sandwich'
-            file.write(reason, () => { reject(reason) })
+            reject(reason) 
         }
     })
 }
@@ -98,23 +77,22 @@ const wait = ms => {
 let attemptCount = 0
 const makeSandwich = async () => {
     while (failed) {
-        attemptCount ++
-        console.log(`Trying to make a sandwich... attempt n. ${attemptCount}`)
-        file.write(`\nAttempt ${attemptCount}\n\n`)
+        attemptCount++
+        displayToDom(`Trying to make a sandwich... attempt n. ${attemptCount}`)
     try {
         await getBread()
         await addFillings()
         await toastSandwich()
         await serveSandwich()
         failed = false
+        displayToDom(`Total attempts: ${attemptCount}\n\n`)
     }
     catch (err) {
-        console.log('An incident occurred:', err)
-        console.log('Trying again in 1 second...')
+        displayToDom(`An incident occurred : ${err}\n Trying again in 1 second...`)
         await wait(1000)
     }
     }
-    file.write(`Total attempts: ${attemptCount}\n\n`)
+  
     try {
         await organizeFails()
     }
@@ -122,6 +100,8 @@ const makeSandwich = async () => {
         console.log(err)
     }
 }
-makeSandwich()
+const button = document.getElementById('generationButton')
+button.addEventListener('click', makeSandwich)
+
 
 
